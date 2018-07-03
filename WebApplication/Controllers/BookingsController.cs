13c600a41f711c5e976bec0d5ed1f9193894ff43
Dillan Carroll <spaced92@gmail.com>
2018-07-03 13:04:00 +1000

    Add project files.

---
+++ WebApplication/Controllers/BookingsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private challengedatabaseEntities db = new challengedatabaseEntities();
+
+        // GET: Bookings
+        public async Task<ActionResult> Index()
+        {
+            var bookings = db.Bookings.Include(b => b.Client).Include(b => b.TourEvent);
+            return View(await bookings.ToListAsync());
+        }
+
+        // GET: Bookings/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = await db.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // GET: Bookings/Create
+        public ActionResult Create()
+        {
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "firstName");
+            ViewBag.eventID = new SelectList(db.TourEvents, "eventID", "eventMonth");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "bookingID,payment,dateBooked,clientID,eventID")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bookings.Add(booking);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "firstName", booking.clientID);
+            ViewBag.eventID = new SelectList(db.TourEvents, "eventID", "eventMonth", booking.eventID);
+            return View(booking);
+        }
+
+        // GET: Bookings/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = await db.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "firstName", booking.clientID);
+            ViewBag.eventID = new SelectList(db.TourEvents, "eventID", "eventMonth", booking.eventID);
+            return View(booking);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "bookingID,payment,dateBooked,clientID,eventID")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(booking).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "firstName", booking.clientID);
+            ViewBag.eventID = new SelectList(db.TourEvents, "eventID", "eventMonth", booking.eventID);
+            return View(booking);
+        }
+
+        // GET: Bookings/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = await db.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Booking booking = await db.Bookings.FindAsync(id);
+            db.Bookings.Remove(booking);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication/Controllers/TourEventsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+using NLog;
+
+namespace WebApplication.Controllers
+{
+    public class TourEventsController : Controller
+    {
+        private challengedatabaseEntities db = new challengedatabaseEntities();
+        private static NLog.Logger nlogger = NLog.LogManager.GetCurrentClassLogger();
+        // GET: TourEvents
+        public ActionResult Index()
+        {
+            try
+            {
+                var tourEvents = db.TourEvents.Include(t => t.Tour);
+                return View(tourEvents.ToList());
+            }
+            catch(Exception ex)
+            {
+                nlogger.Trace("Index view found");
+                throw ex;
+            }
+        }
+
+        // GET: TourEvents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TourEvent tourEvent = db.TourEvents.Find(id);
+            if (tourEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tourEvent);
+        }
+
+        // GET: TourEvents/Create
+        public ActionResult Create()
+        {
+            ViewBag.tourID = new SelectList(db.Tours, "tourID", "name");
+            return View();
+        }
+
+        // POST: TourEvents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "eventID,eventDay,eventMonth,eventYear,fee,tourID")] TourEvent tourEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TourEvents.Add(tourEvent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.tourID = new SelectList(db.Tours, "tourID", "name", tourEvent.tourID);
+            return View(tourEvent);
+        }
+
+        // GET: TourEvents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TourEvent tourEvent = db.TourEvents.Find(id);
+            if (tourEvent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.tourID = new SelectList(db.Tours, "tourID", "name", tourEvent.tourID);
+            return View(tourEvent);
+        }
+
+        // POST: TourEvents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "eventID,eventDay,eventMonth,eventYear,fee,tourID")] TourEvent tourEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tourEvent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.tourID = new SelectList(db.Tours, "tourID", "name", tourEvent.tourID);
+            return View(tourEvent);
+        }
+
+        // GET: TourEvents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TourEvent tourEvent = db.TourEvents.Find(id);
+            if (tourEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tourEvent);
+        }
+
+        // POST: TourEvents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TourEvent tourEvent = db.TourEvents.Find(id);
+            db.TourEvents.Remove(tourEvent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
